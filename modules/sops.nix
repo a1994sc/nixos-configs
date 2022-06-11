a04d62fe96ca9c37e5641d2c01e9bb4c902d1d13
Allen Conlon <allen@conlon.dev>
2022-06-10 23:58:52 -0400

    Updated sops to pin it to current version.

--- modules/sops.nix
+++ modules/sops.nix
@@ -1,7 +1,14 @@
 { config, pkgs, lib, ... }:
-
-{
-  imports = [ "${builtins.fetchTarball "https://github.com/Mic92/sops-nix/archive/master.tar.gz"}/modules/sops" ];
+let
+  rev = "f075361ecbde21535b38e41dfaa28a28f160855c";
+#  src = fetchFromGitHub {
+#    owner = "Mic92";
+#    repo = "sops-nix";
+#    rev = "f075361ecbde21535b38e41dfaa28a28f160855c";
+#    sha256 = "sha256-uCdQ2fzIPGakHw2TkvOncUvCl7Fo7z/vagpDWYooO7s=";
+#  };
+in {
+  imports = [ "${builtins.fetchTarball "https://github.com/Mic92/sops-nix/archive/${rev}.tar.gz"}/modules/sops" ];
 
   environment.systemPackages = with pkgs; [
     age
