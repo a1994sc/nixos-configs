3cd59dbec394feec46548dc10fb5b42760559fbc
Allen Conlon <allen@conlon.dev>
2022-06-12 15:24:23 -0400

    Updated teh SHA256 of k3s-amd64

--- pkgs/k3s-amd64.nix
+++ pkgs/k3s-amd64.nix
@@ -3,7 +3,7 @@ pkgs.stdenv.mkDerivation {
   name = "k3s";
   src = pkgs.fetchurl {
     url = "https://github.com/k3s-io/k3s/releases/download/v1.23.7%2Bk3s1/k3s";
-    sha256 = "6b015a1d6004f097035f295dfbd9ef20c4af573be5f3cd46c2e7c578804906e7";
+    sha256 = "82b6fa248db2221f4df0c7ae6db353742bd98cf1b6c478830f18f36e195069cc";
   };
   phases = [ "installPhase" ];
   installPhase = ''
