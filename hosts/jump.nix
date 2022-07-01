79d4b776b3fef826972c26f0eb18205fd4caa7d0
Allen Conlon <allen@conlon.dev>
2022-07-01 19:16:52 -0400

    Added regular jump user to Jump Host

--- hosts/jump.nix
+++ hosts/jump.nix
@@ -23,10 +23,27 @@
     helm
   ];
 
+  users.users.jump = {
+    isNormalUser = true;
+    extraGroups  = [];
+    openssh.authorizedKeys.keys =  [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMpoTjm581SSJi51VuyDXkGj+JThQOavxicFgK1Z/YlN pihole@adrp.xyz"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICVflfgY9+i0jlwcHlVvONUIFyDN3ynU0sEF0nv4nFrw phone@adrp.xyz"
+    ];
+  };
+
   sops.secrets.jump = {
     owner = "ascii";
     format = "binary";
     path = "/home/ascii/.ssh/jump";
+    sopsFile = /etc/nixos/secrets/jump.yaml;
+    mode = "0600";
+  };
+
+  sops.secrets."jump-jump" = {
+    owner = "jump";
+    format = "binary";
+    path = "/home/jump/.ssh/jump";
     sopsFile = /etc/nixos/secrets/jump.yaml;
     mode = "0600";
   };
