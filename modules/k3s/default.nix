d458c4f9d6233a4693d98e0ba1b5a85f6935c28b
Allen Conlon <allen@conlon.dev>
2022-09-05 09:39:21 -0400

    Updated the kernel modules for Istio and Ceph

--- modules/k3s/default.nix
+++ modules/k3s/default.nix
@@ -9,7 +9,38 @@
 
     environment.systemPackages = with pkgs; [
       k3s
+      lvm2
       # cri-tools
+    ];
+
+    boot.kernelModules = [
+      # Istio modules
+      "br_netfilter"
+      "ip6table_mangle"
+      "ip6table_nat"
+      "ip6table_raw"
+      "iptable_mangle"
+      "iptable_nat"
+      "iptable_raw"
+      "xt_REDIRECT"
+      "xt_connmark"
+      "xt_conntrack"
+      "xt_mark"
+      "xt_owner"
+      "xt_tcpudp"
+      "bridge"
+      "ip6_tables"
+      "ip_tables"
+      "nf_conntrack"
+      "nf_conntrack_ipv4"
+      "nf_conntrack_ipv6"
+      "nf_nat"
+      "nf_nat_ipv4"
+      "nf_nat_ipv6"
+      "nf_nat_redirect"
+      "x_tables"
+      # Rook Ceph module
+      "rbd"
     ];
 
     networking.extraHosts =
