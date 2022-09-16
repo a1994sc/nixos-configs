7081cb2ab044ac3f4434e935284750607d8bb4ec
Allen Conlon <allen@conlon.dev>
2022-09-16 15:32:26 -0400

    Updated trunk00

--- hosts/trunk00.nix
+++ hosts/trunk00.nix
@@ -63,7 +63,7 @@ in {
         "${pkgs.k3s}/bin/k3s server"
         "--tls-san 10.2.1.9"
         "--cluster-init"
-        "--server https://10.2.1.9:6443"
+#        "--server https://10.2.1.9:6443"
         "--token-file ${config.sops.secrets.token.path}"
         "--disable traefik,servicelb,coredns,metrics-server"
         "--write-kubeconfig-mode=644"
