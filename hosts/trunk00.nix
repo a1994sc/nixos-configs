2328b8bb31269a922216dab7dab58b9210c86f62
Allen Conlon <allen@conlon.dev>
2022-09-17 16:41:05 -0400

    Fixed issue for trunk00

--- hosts/trunk00.nix
+++ hosts/trunk00.nix
@@ -7,6 +7,23 @@
       shutdownGracePeriod: 30s
       shutdownGracePeriodCriticalPods: 10s
     '';
+  k3s-args = toString [
+    "${pkgs.k3s}/bin/k3s server"
+    "--tls-san 10.2.1.9"
+    "--cluster-init"
+    "--token-file ${config.sops.secrets.token.path}"
+    "--disable traefik,servicelb,metrics-server"
+    "--write-kubeconfig-mode=644"
+    "--kube-apiserver-arg default-not-ready-toleration-seconds=30"
+    "--kube-apiserver-arg default-unreachable-toleration-seconds=30"
+    "--kube-apiserver-arg feature-gates=GracefulNodeShutdown=true"
+    "--kube-controller-arg node-monitor-period=20s"
+    "--kube-controller-arg node-monitor-grace-period=20s"
+    "--kubelet-arg node-status-update-frequency=5s"
+    "--kube-apiserver-arg feature-gates=GracefulNodeShutdownBasedOnPodPriority=true"
+    "--kubelet-arg=config=${kubeletConfig}"
+    "--container-runtime-endpoint unix:///run/containerd/containerd.sock"
+  ];
 in {
   imports =
     [ 
@@ -22,21 +39,5 @@ in {
 
   system.autoUpgrade.dates = "Fri 04:00";
 
-  systemd.services.k3s-server.serviceConfig.ExecStart = pkgs.lib.mkForce ''
-    ${pkgs.k3s}/bin/k3s server
-    --tls-san 10.2.1.9
-    --cluster-init
-    --token-file ${config.sops.secrets.token.path}
-    --disable traefik,servicelb,metrics-server
-    --write-kubeconfig-mode=644
-    --kube-apiserver-arg default-not-ready-toleration-seconds=30
-    --kube-apiserver-arg default-unreachable-toleration-seconds=30
-    --kube-apiserver-arg feature-gates=GracefulNodeShutdown=true
-    --kube-controller-arg node-monitor-period=20s
-    --kube-controller-arg node-monitor-grace-period=20s
-    --kubelet-arg node-status-update-frequency=5s
-    --kube-apiserver-arg feature-gates=GracefulNodeShutdownBasedOnPodPriority=true
-    --kubelet-arg=config=${kubeletConfig}
-    --container-runtime-endpoint unix:///run/containerd/containerd.sock
-    '';
+  systemd.services.k3s-server.serviceConfig.ExecStart = pkgs.lib.mkForce k3s-args;
 }
