287456bea816d72a11bbe0a790413cda7450de71
Allen Conlon <allen@conlon.dev>
2022-10-28 11:09:45 -0400

    Added vault secret

--- hosts/jump.nix
+++ hosts/jump.nix
@@ -98,6 +98,13 @@ in {
     mode = "0600";
   };
 
+   sops.secrets.vault = {
+    owner = "ascii";
+    path = "/home/ascii/.ssh/vault";
+    sopsFile = /etc/nixos/secrets/vault.yaml;
+    mode = "0600";
+  };
+
   sops.secrets.jump = {
     owner = "jump";
     path = "/home/jump/.ssh/jump";
