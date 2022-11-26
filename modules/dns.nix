82cf77685221b3208a88280791d278d9840d68e9
Allen Conlon <allen@conlon.dev>
2022-11-25 20:47:58 -0500

    Update docker files

--- modules/dns.nix
+++ modules/dns.nix
@@ -1,6 +1,10 @@
 { config, pkgs, lib, ... }:
 
 {
+  imports = [
+    /etc/nixos/modules/database.nix
+  ];
+
   services.powerdns.enable = true;
 
   sops.secrets = {
@@ -18,12 +22,26 @@
     };
   };
 
+  services.mysql.initialDatabases.powerdns.name = "powerdns";
+  services.mysql.ensureUsers.powerdns = {
+    name = "powerdns";
+    ensurePermissions = {
+      "${config.services.mysql.ensureUsers.powerdns.name}.*" = "ALL PRIVILEGES";
+    };
+  };
+
   services.powerdns-admin = {
     enable = true;
     saltFile = "/run/secrets/powerdnssalt";
     secretKeyFile = "/run/secrets/powerdns";
     config = ''
       PORT = 9191
+      SQLALCHEMY_DATABASE_URI = 'mysql://{}:{}@{}/{}'.format(
+         urllib.parse.quote_plus(${config.services.mysql.ensureUsers.powerdns.name}),
+         urllib.parse.quote_plus(),
+         127.0.0.1,
+         ${config.services.mysql.initialDatabases.powerdns.name}
+      )
     '';
   };
 
---
+++ modules/docker/pihole-sync-trunk.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, lib, ... }:
+
+{
+  sops.secrets = {
+    pihole = {
+      sopsFile = /etc/nixos/modules/docker/secrets/seedling/pihole.yaml;
+      mode = "0644";
+    };
+  };
+
+  systemd.services."docker-compose@pihole-sync-trunk" = {
+    enable = true;
+    unitConfig = {
+      Description = "%i service with docker compose";
+      PartOf = "docker.service";
+      After = "docker.service";
+    };
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      WorkingDirectory = "/etc/nixos/compose/%i";
+      ExecStart = "${pkgs.docker-compose}/bin/docker-compose up -d --remove-orphans";
+      ExecStop = "${pkgs.docker-compose}/bin/docker-compose down";
+    };
+  };
+}
