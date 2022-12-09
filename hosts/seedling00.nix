b5637e81432d791d6966edede4e349d393ce508c
Allen Conlon <allen@conlon.dev>
2022-12-09 14:08:23 -0500

    added seedling systems

---
+++ hosts/seedling00.nix
@@ -0,0 +1,25 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [
+      /etc/nixos/modules/main-config.nix
+      /etc/nixos/modules/bare.nix
+      # /etc/nixos/modules/tailscale.nix
+      /etc/nixos/modules/sops.nix
+      /etc/nixos/modules/docker.nix
+      # /etc/nixos/modules/docker/pihole-sync-trunk.nix
+    ];
+
+  networking.hostName = "seedling00";
+
+  nix.gc.dates = "Mon 02:00";
+
+  system.autoUpgrade.dates = "Mon 04:00";
+
+  networking.interfaces.eth0.ipv4.addresses = [ {
+    address = "10.2.1.6";
+    prefixLength = 24;
+  } ];
+
+}
---
+++ hosts/seedling01.nix
@@ -0,0 +1,25 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [
+      /etc/nixos/modules/main-config.nix
+      /etc/nixos/modules/bare.nix
+      # /etc/nixos/modules/tailscale.nix
+      /etc/nixos/modules/sops.nix
+      /etc/nixos/modules/docker.nix
+      # /etc/nixos/modules/docker/pihole-sync-leaf.nix
+    ];
+
+  networking.hostName = "seedling01";
+
+  nix.gc.dates = "Fri 02:00";
+
+  system.autoUpgrade.dates = "Fri 04:00";
+
+  networking.interfaces.eth0.ipv4.addresses = [ {
+    address = "10.2.1.7";
+    prefixLength = 24;
+  } ];
+
+}
