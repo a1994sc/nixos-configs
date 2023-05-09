70291eda5ed6aba474c0cf5956d7d1dc9c6e1da1
Allen Conlon <allen@conlon.dev>
2023-05-09 05:32:00 -0400

    Added kube claim for step ca

--- modules/step-ca.nix
+++ modules/step-ca.nix
@@ -56,6 +56,20 @@ in {
             x = "Ipkyt1hJ0a5jpdFvkm97mQTF3m5JPg78izuK8XyILaE";
             y = "lgQ4iOHfIaEElyy9GOOq0BsQGYx4MCLiQqjWbrM4eTw";
           };
+        }
+        {
+          type = "JWK";
+          name = "kube@adrp.xyz";
+          encryptedKey = "eyJhbGciOiJQQkVTMi1IUzI1NitBMTI4S1ciLCJjdHkiOiJqd2sranNvbiIsImVuYyI6IkEyNTZHQ00iLCJwMmMiOjEwMDAwMCwicDJzIjoiUENBN011djZmMVF6TVBVOEpwS2FYdyJ9.aAuxNbFJCxPEbfRwDMJxmQGQRhzVUh7KWwimuzDrr6k-dY1_3-_AVQ.-Qjts0c27pA0g9Yn.2aI-1HPGWFL8VxC0I8WVMHd6nHjkavI0aN99aZWA7Q02oR81aeMIU8v8Dm68PPWvn8no5LiD9abbGY_lQM96qfZOQoyU6jMmyAbWYNUcMi2gqJNZU-ckjrUDnTVzNTCZ8ARDj8j79ZoMM-g9n2qVW1PnSUhk1FzvXeRtMZBIXtmLNunPu8vmuvPpKcBfHagIeVVUCEeJmBodjB9m8doOly-jMbAagT5cB2tOFvpmMfj0N7CrzHUr7nicW5kgmTKpM-GAcyVTPI2bvDVwfjMcekwKhn66kMpeys5XTJRqikUK-Pxr45R-HzhyDcYrdsnii6hIoKhjNB5XQah3kyo.C_jjxBwZyD-jD8FHk9eIPw";
+          key = {
+            use = "sig";
+            kty = "EC";
+            kid = "p2-tlIQtHRJdfwu2TzaPrQcY0o7oZYqTXi-194stWhE";
+            crv = "P-256";
+            alg = "ES256";
+            x = "iFfNBs_UasvrNs050b4DqlRqE79XmB70mWpZbrS4kas";
+            y = "zc7u6gfL3-xpNzM7jzsn7BCOTLOlvAhAJheY72uy4Sg";
+          };
         }];
       };
     };
