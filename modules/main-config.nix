a21ab867ae0863f89d2e5fb317a96f507b89ba50
Allen Conlon <allen@conlon.dev>
2023-12-16 08:06:55 -0500

    Updated main config

--- modules/main-config.nix
+++ modules/main-config.nix
@@ -8,6 +8,15 @@ in {
   programs.bash.enableCompletion   = true;
   services.udev.packages           = [ pkgs.yubikey-personalization ];
 
+  environment.variables            = {
+    HISTFILESIZE                   = "";
+    HISTSIZE                       = "";
+    HISTTIMEFORMAT                 = "[%F %T] ";
+    HISTCONTROL                    = "ignoredups";
+    HISTFILE                       = "$HOME/.bash_eternal_history";
+    PROMPT_COMMAND                 = "history -a; history -c; history -r; $PROMPT_COMMAND";
+  };
+
   networking                       = {
     domain                         = "adrp.xyz";
     search                         = [ "adrp.xyz" ];
@@ -51,6 +60,7 @@ in {
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ+ijO8h19n6GB9am9cek91WjLvpn80w3Y5XthK3Tpo/ jump@adrp.xyz"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPEVDFj/DsBQNjAoid6lbcJhWWyx5Gq6VzSJGKvK+bR6 pixel7@adrp.xyz"
       "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAF/R3bjaZYUB6cJe7jcexHc+n+zk+F+39SH55nHWk1uqX5h+/YSkDlDPl42QfVVcV/kyX21yv3zUO3zl6h+OsDltgH9+VggOJSvrYYWLx5vb9H3gH6y3yfc2V8Eyg6v4svSE2z6SbRmQw/bLmCcCU+C+oC74du/a/VJocT4ib706LMG2A== aconlon@omga.ardp.xyz"
+      "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBACa5MIyu4mLLLc0D5Y0eOWV1JnvvSo68pDJAh4SyC1WyMVK1eOIlpyDlfFNu7wev8fPELJEwbT+pCsjH2FVU8qRNAH17nW1EBn9xWOX7rEnpxOp6X485+jeA0t/a2jB6e7Bcn86Xwa1tPEbIKS6eo530KMLagaCFpl9arv1SGWeh6/YAw== aconlon@puck.adrp.xyz"
     ];
   };
 
